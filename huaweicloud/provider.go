a9e199e3225655bb0e00a0ffd1b767d9e9e85344
ruwenqiang123 <ruwenqiang123@outlook.com>
2026-03-27 10:03:47 +0800

    feat(elb): add new data source to query the list of listener tags

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1834,6 +1834,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_elb_loadbalancer_ports":                  elb.DataSourceElbLoadBalancerPorts(),
 			"huaweicloud_elb_loadbalancer_status":                 elb.DataSourceElbLoadBalancerStatus(),
 			"huaweicloud_elb_listeners":                           elb.DataSourceElbListeners(),
+			"huaweicloud_elb_listener_tags":                       elb.DataSourceListenerTags(),
 			"huaweicloud_elb_members":                             elb.DataSourceElbMembers(),
 			"huaweicloud_elb_all_members":                         elb.DataSourceElbAllMembers(),
 			"huaweicloud_elb_ipgroups":                            elb.DataSourceElbIpGroups(),
