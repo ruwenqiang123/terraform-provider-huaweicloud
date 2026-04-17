40468152c18be59dbe44d614da9de5018c00742f
jinyangyang222 <jinyangyang222@outlook.com>
2026-04-17 11:41:03 +0800

    feat(rfs): support `rfs_private_hook_versions` data source (#9354)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2019,6 +2019,7 @@ func Provider() *schema.Provider {
 
 			"huaweicloud_rfs_private_module_versions": rfs.DataSourcePrivateModuleVersions(),
 			"huaweicloud_rfs_private_modules":         rfs.DataSourcePrivateModules(),
+			"huaweicloud_rfs_private_hook_versions":   rfs.DataSourcePrivateHookVersions(),
 			"huaweicloud_rfs_stack_instances":         rfs.DataSourceStackInstances(),
 			"huaweicloud_rfs_private_providers":       rfs.DataSourcePrivateProviders(),
 			"huaweicloud_rfs_templates":               rfs.DataSourceRfsTemplates(),
