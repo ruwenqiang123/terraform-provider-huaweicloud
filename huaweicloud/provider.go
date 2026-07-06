5675b64cc732601d44a5e8cae63fee1fbdb771fa
allium <deerhang@163.com>
2026-07-06 15:25:02 +0800

    feat(dsc): new datasource to query scan jobs (#10097)

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -1415,6 +1415,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dsc_event_overview":       dsc.DataSourceDscEventOverview(),
 			"huaweicloud_dsc_threat_trend":         dsc.DataSourceDscThreatTrend(),
 			"huaweicloud_dsc_database_instances":   dsc.DataSourceDatabaseInstances(),
+			"huaweicloud_dsc_scan_jobs":            dsc.DataSourceScanJobs(),
 			"huaweicloud_dsc_multi_accounts":       dsc.DataSourceMultiAccounts(),
 			"huaweicloud_dsc_multi_organizations":  dsc.DataSourceMultiOrganizations(),
 
